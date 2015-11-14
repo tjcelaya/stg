a9bc5dbc5138922da6ebe51f042ecaaf26d179b9
Tomas Celaya <tjcelaya@gmail.com>
2015-11-14 17:14:58 -0500

    redux connect example

--- apps/stg_site/priv/static/js/components/about.js
+++ apps/stg_site/priv/static/js/components/about.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-class About extends React.Component {
+// as an example, we can connect this non-root component to the redux store and have it
+// consume the store's time with a different value
+@connect((state) => ({ thetime: state.time }))
+class About extends Component {
     constructor(props) {
         super(props);
         this.displayName = 'About';
     }
     render() {
-      let { id } = this.props.params
-      let { query } = this.props.location
+      // this amounts to
+      // let id = this.props.params.id
+      // let query = this.props.location.query
+      // let aboutTime = this.props.thetime
+      //
+      let { params: { id }, location: { query }, thetime: aboutTime } = this.props
       let age = query && query.showAge ? '33' : ''
 
       return (
         <div className="User">
           <h1>User id: {id}</h1>
-          {age}
+          {aboutTime}
         </div>
       )
     }
--- apps/stg_site/priv/static/js/components/root.js
+++ apps/stg_site/priv/static/js/components/root.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router';
 
 import { MenuItem, LeftNav, Mixins, Styles } from 'material-ui';
 
+// if you want to get fancy and use destructuring at the same time, this could be
+// @connect(({ time }) => ({ time }))
+// but we've chosen to pull props out of state to be explicit
+
 @connect((state) => ({ time: state.time }))
 class Root extends Component {
   render() {
@@ -14,6 +18,7 @@ class Root extends Component {
           <MenuItem index='0'>{this.props.time}</MenuItem>
           <MenuItem index='1'><Link to='/' activeClassName='active'>Root</Link></MenuItem>
           <MenuItem index='2'><Link to='/about' activeClassName='active'>About</Link></MenuItem>
+          <MenuItem index='2'><Link to='/about/12' activeClassName='active'>About 12</Link></MenuItem>
         </LeftNav>
         <div>{this.props.children || <h2>Nothing yet</h2>}</div>
       </div>
